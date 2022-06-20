949f72335d32e95391816a047a78f44bbd1d36f5
Riyadh Alkhanin <63449062+MrRiybot@users.noreply.github.com>
2022-06-20 11:30:42 +0300

    Make it usable

--- add.py
+++ add.py
@@ -11,16 +11,15 @@ class ADD:
 
         driver = webdriver.Chrome(executable_path=('./' + driverpath))
         driver.get("https://edugate.ksu.edu.sa/ksu/init")
-        input(' meow!')
-        while True:
+        time.sleep(2)
+        section = input('number of section:')
+        state = True
+        while state:
             action = driver.find_element_by_id('saveBut1')
             action.click()
             time.sleep(1)
-            action = driver.find_element_by_id('C111_1_1')
-            action.click()
-            time.sleep(1)
-            action = driver.find_element_by_id('allSection111_1_1_1')
-            action.send_keys('5582')
+            action = driver.find_element_by_xpath('//*[@id="addSection0"]')
+            action.send_keys(section)
             action = driver.find_element_by_xpath('//*[@id="left"]/div[3]/form/table[5]/tbody/tr[2]/td/table/tbody/tr/td[4]/a')
             action.click()
             time.sleep(2)
@@ -28,15 +27,8 @@ class ADD:
                 action = driver.find_element_by_xpath('//*[@id="successAddDiv"]/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a')
                 action.click()
                 for i in range(5):
+                    state = False
                     winsound.Playsound('alarm.wav')
-                break
             except:
-                time.sleep(1)
-                action = driver.find_element_by_xpath('//*[@id="errorAddDiv"]/table/tbody/tr[1]/td/table/tbody/tr/td/a')
-                action.click()
-
-
-
-
-
+                continue
         return driver
