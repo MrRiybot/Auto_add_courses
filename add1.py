b8b4a4abf9ec194d84d398b0462068d7fdf13a27
Riyadh Alkhanin <63449062+MrRiybot@users.noreply.github.com>
2022-08-23 12:54:21 +0300

    now you can try to add more than one course

--- add1.py
+++ add1.py
@@ -11,18 +11,23 @@ class ADD:
 
         driver = webdriver.Chrome(executable_path=('./' + driverpath))
         driver.get("https://edugate.ksu.edu.sa/ksu/init")
-        time.sleep(2)
-        section = input('number of section:')
+        time.sleep(5)
+        sections = list()
+        number_of_sections = int(input("how many sections that you want to try to add?: "))
+        for i in range(number_of_sections):
+            section = input('section number {0}:'.format(i))
+            sections.append(section)
         state = True
         while state:
-            action = driver.find_element_by_id('saveBut1')
-            action.click()
-            time.sleep(1)
-            action = driver.find_element_by_xpath('//*[@id="addSection0"]')
-            action.send_keys(section)
-            action = driver.find_element_by_xpath('//*[@id="left"]/div[3]/form/table[5]/tbody/tr[2]/td/table/tbody/tr/td[4]/a')
-            action.click()
-            time.sleep(2)
+            for s in sections:
+                action = driver.find_element_by_id('saveBut1')
+                action.click()
+                time.sleep(1)
+                action = driver.find_element_by_xpath('//*[@id="addSection0"]')
+                action.send_keys(s)
+                action = driver.find_element_by_xpath('//*[@id="left"]/div[3]/form/table[5]/tbody/tr[2]/td/table/tbody/tr/td[4]/a')
+                action.click()
+                time.sleep(2)
             try:
                 action = driver.find_element_by_xpath('//*[@id="successAddDiv"]/table/tbody/tr[1]/td/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a')
                 action.click()
